8e7f3930c81ec983e4ee752daf77a56e2ce6414d
Desiree Morris <desiquinncompany@gmail.com>
2019-12-05 17:19:56 -0500

    completed file_io

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -4,11 +4,24 @@ at how to read and write to files here:
 
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
+#reading notes
+# modes
+# r - read (default)
+# w - write
+# a - append (data written at the end)
+# r+ - read and 
+# add b - opens in binary mode for files that have more than just text
+# a+ - creates file if it doesn't exsist and opens it in append mode
+
 
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open('foo.txt') as foo:
+    print(foo.read())
+
+# foo.read() checking to see if file closed, should return ValueError
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +29,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+with open('bar.txt', 'w+') as bar:
+    bar.write("I am writing a bar.txt file")
+
+with open('bar.txt') as bar:
+    print(bar.read())
