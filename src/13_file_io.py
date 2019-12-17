62166792ecfe1ac88872ec93d4089bf058a3f65b
Desiree Morris <desiquinncompany@gmail.com>
2019-12-17 14:31:16 -0500

    added more lines of code to the bar.txt file

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -31,7 +31,7 @@ with open('foo.txt') as foo:
 # YOUR CODE HERE
 
 with open('bar.txt', 'w+') as bar:
-    bar.write("I am writing a bar.txt file")
+    bar.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
 
 with open('bar.txt') as bar:
     print(bar.read())
